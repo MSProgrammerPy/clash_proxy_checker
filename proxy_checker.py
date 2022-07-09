3a63f202cbd1ad286c581aa1238d6f2152c3ecfd
MSProgrammerPy <58591962+MSProgrammerPy@users.noreply.github.com>
2022-07-09 20:34:08 +0430

    _validate_proxy function was defined in ProxyChecker class. This function validates that the given proxy is not a proxy group.

--- proxy_checker.py
+++ proxy_checker.py
@@ -28,3 +28,15 @@ class ProxyChecker:
         """
         data: dict = requests.get(url=f"{self.api}proxies/{proxy_name}", headers=self.auth_headers).json()
         return data
+
+    def _validate_proxy(self, proxy_name: str) -> bool:
+        """
+        Validates that the proxy is not a proxy group.
+
+        Returns True if the proxy is not a proxy group, False otherwise.
+
+        :param proxy_name: proxy name.
+        :return: bool
+        """
+        proxy: dict = self.get_proxy_details(proxy_name=proxy_name)
+        return proxy["type"] not in ("Direct", "Reject", "select", "url-test", "relay", "fallback")
