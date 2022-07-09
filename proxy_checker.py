d2037d92fecf556907d82b6a78ee0d981156d2ff
MSProgrammerPy <58591962+MSProgrammerPy@users.noreply.github.com>
2022-07-09 19:56:35 +0430

    get_proxy_details function was defined in ProxyChecker class. This function takes the proxy name and returns proxy details.

--- proxy_checker.py
+++ proxy_checker.py
@@ -1,3 +1,6 @@
+import requests
+
+
 class ProxyChecker:
     def __init__(
             self,
@@ -15,3 +18,13 @@ class ProxyChecker:
         self.auth_headers: dict = {
             "Authorization": f"Bearer {secret}"
         }
+
+    def get_proxy_details(self, proxy_name: str) -> dict:
+        """
+        Returns proxy details.
+
+        :param proxy_name: proxy name.
+        :return: proxy details.
+        """
+        data: dict = requests.get(url=f"{self.api}proxies/{proxy_name}", headers=self.auth_headers).json()
+        return data
